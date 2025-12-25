8f23ff09625b93a660ced8af040b09733db0791d
Rohit Kadam <upskillrohit@gmail.com>
2025-12-25 19:28:03 +0530

    footer

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Info from '../components/Info.jsx'
-import Footer from '../components/footer.jsx'
+import Footer from '../components/Footer.jsx'
 import HomeTabs from '../components/Hometabs.jsx'
 import Features from '../components/Features.jsx'
 
